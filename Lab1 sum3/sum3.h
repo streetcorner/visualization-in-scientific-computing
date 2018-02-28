a082c6bccc5b05ec8b9ed95a019d7a5990856eef
Wang Yumeng <415724219@qq.com>
2018-02-28 17:31:16 +0800

    Add files via upload

---
+++ Lab1 sum3/sum3.h
@@ -0,0 +1,40 @@
+
+// sum3.h : sum3 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// Csum3App:
+// 有关此类的实现，请参阅 sum3.cpp
+//
+
+class Csum3App : public CWinAppEx
+{
+public:
+	Csum3App();
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	UINT  m_nAppLook;
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Csum3App theApp;
